e44a4b88d00f19b8975d57e0e20d5a3a2809cc98
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2024-11-18 13:31:09 +0400

    Add mult operation and 'bye,bye'

--- calc.js
+++ calc.js
@@ -2,6 +2,7 @@ x = Number(prompt('x'));
 y = Number(prompt('y'));
 console.log(x + y);
 console.log(x - y);
+console.log(x * y);
 console.(x ** y);
 
 if(y === 0){
@@ -9,3 +10,4 @@ if(y === 0){
 }else {
   console.log( x / y);
 }
+console.log('BYE , BYE');
